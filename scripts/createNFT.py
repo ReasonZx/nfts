2fcb900f82cd19bcb4296082d1e5437d99fe5c69
ReasonZx <xico.reasonzx@gmail.com>
2023-06-30 23:10:36 +0200

    script to mint NFTs

---
+++ scripts/createNFT.py
@@ -0,0 +1,20 @@
+from brownie import simpleNFT, config, network
+from scripts import helpfulScripts, deploy
+from web3 import Web3
+
+
+
+def createNft(account, nftSC, country):
+    tx = nftSC.createNFT(config["jsonNftURI"][country], {"from": account})
+    tx.wait(1)
+    return tx
+    
+def main():
+    account = helpfulScripts.getAccount()
+
+    if ("fork" in network.show_active()) :
+        nftSC = deploy.deployNftSC()
+    else:  
+        nftSC = simpleNFT[-1]
+
+    createNft(account, nftSC, "ch")
